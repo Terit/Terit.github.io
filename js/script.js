9a4a78eee0d389e5a24b5752325c5abae2a0b9cf
Andy Theriault <artheriault@gmail.com>
2015-03-21 15:41:05 -0700

    single paged

---
+++ js/script.js
@@ -0,0 +1,35 @@
+$(function() {
+
+  $('#fullpage').fullpage({
+    css: true,
+    anchors: ['firstPage', 'secondPage', 'thirdPage'],
+  
+    onLeave: function(anchorLink, index){
+      //using index
+      if(index == '1'){
+        $('header').removeClass('topped');
+        $('.fa-arrow-up').replaceWith('<i class="fa fa-home"></i>');
+        $('#home').addClass('underline')
+      } else {
+        $('header').addClass('topped');
+        $('.section').addClass('topped');
+        $('.fa-home').replaceWith('<i class="fa fa-arrow-up"></i>');
+        $('#home').removeClass('underline')
+      }
+      if(index == '3'){
+        $('#portfolio').addClass('underline');
+        $('.fa-folder').replaceWith('<i class="fa fa-folder-open"></i>');
+      } else {
+        $('#portfolio').removeClass('underline');
+        $('.fa-folder-open').replaceWith('<i class="fa fa-folder"></i>');
+      }
+    }
+  });
+
+  $('#portrait').on('mouseover', function() {
+    $('#about').removeClass('hide');
+  }).on('mouseleave', function() {
+    $('#about').addClass('hide');
+  });
+
+});
