5e5c68e497842622d5bd417caa82136410732d6a
Terit <artheriault@gmail.com>
2016-04-10 13:22:52 -0700

    remove original folder from branch

---
+++ webpack.config.prod.js
@@ -0,0 +1,57 @@
+var webpack = require('webpack');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: ['babel-polyfill', './src/index.js', 'webpack-dev-server/client?http://localhost:8080'],
+    resume: ['babel-polyfill', './src/resume.js', 'webpack-dev-server/client?http://localhost:8080'],
+  },
+  output: {
+    path: '/',
+    publicPath: '/',
+    filename: '[name].js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: path.join(__dirname, 'src'),
+        exclude: /(node_modules|original)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react', 'react-hmre'],
+        },
+      },
+      {
+        test: /vendor\/.+\.(jsx|js)$/,
+        loader: 'imports?jQuery=jquery,$=jquery,this=>window',
+      },
+      {
+        test: /\.css$/,
+        includes: [
+          path.join(__dirname, 'src', 'css'),
+          path.join(__dirname, 'src', 'components', 'resume'),
+        ],
+        loader: 'style-loader!css-loader',
+      },
+      {
+        test: /\.(jpg|png)$/,
+        loader: 'url?limit=25000',
+        include: path.join(__dirname, 'images'),
+      },
+      {
+        test: /\.json$/,
+        include: /src/,
+        loader: 'json-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      inject: true,
+    }),
+    new webpack.optimize.UglifyJsPlugin(),
+  ],
+};
