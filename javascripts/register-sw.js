75e7b52e07c904ddc4603dc69260b5cb6aadbba3
Andy Theriault <artheriault@gmail.com>
2016-07-08 23:04:49 -0700

    remove most of react version and started on a header

--- javascripts/register-sw.js
+++ javascripts/register-sw.js
@@ -1,9 +1,8 @@
 var reg;
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js', { scope: '/' }).then(function() {
-    return serviceWorker.ready;
+    return navigator.serviceWorker.ready;
   }).then(function(swRegistration) {
-    console.log('sw registered')
     reg = swRegistration;
   }).catch(function(err) {
     console.log('Oops: ', err)
